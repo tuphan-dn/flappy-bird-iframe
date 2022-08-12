48d4d01436511014735b42761ec6629b632fc027
tuphan-dn <tuphan@descartes.network>
2022-08-12 11:16:32 +0700

    first commit

---
+++ src/view/index.tsx
@@ -0,0 +1,20 @@
+import EmbededView from '@sentre/embeded-view'
+
+import configs from 'configs'
+
+const {
+  manifest: { appId },
+} = configs
+
+const View = () => {
+  return (
+    <EmbededView
+      appId={appId}
+      src={'https://playcanv.as/p/2OlkUaxF/'}
+      title="Flappy Bird: A technical demo of games on Senhub"
+      wallet={window.sentre.wallet}
+    />
+  )
+}
+
+export default View
